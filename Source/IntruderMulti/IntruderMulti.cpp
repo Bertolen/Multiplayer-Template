a69aab5c20e5438c3c5bd29b6cd6c5da56195f50
Bertolen <33809187+Bertolen@users.noreply.github.com>
2019-03-04 23:15:58 +0100

    initial commit
    Current UE version : UE4.21

---
+++ Source/IntruderMulti/IntruderMulti.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "IntruderMulti.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IntruderMulti, "IntruderMulti" );
+ 
+
+DEFINE_LOG_CATEGORY(IntruderDebug);
